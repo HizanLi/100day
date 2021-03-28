51d79ec43315549cad344fee11f99b218f9d4322
linxinl <linxinl@uvic.ca>
2021-03-27 23:40:02 -0700

    update

--- main.py
+++ main.py
@@ -6,15 +6,45 @@ import copy
 import random
 
 
-class game(object):
+# 第一层按钮
+# Study 1 学习
+# Excerise 2 锻炼
+# relax 3 休息
+# invite 4 邀约
 
+# 第二层按钮
+# 学习 +成绩 +压力
+# Study 1-1 做习题
+# Study 1-2 看讲解
+# Study 1-3 请家教
+# Study 1-4 补习班
+
+# 锻炼 -压力 -体力 -金钱？ +体制/敏捷/力量
+# Excerise 2 举铁
+# Excerise 2 长跑
+# Excerise 2 铁人三项
+# Excerise 2 专业训练
+
+# 休息 -压力 -金钱 +体力
+# relax 3 玩手机
+# relax 3 去网吧
+# relax 3 逛超市
+# relax 3 玩电脑
+
+# 邀约 +好感 -金钱
+# invite 4 图书馆
+# invite 4 大商场
+# invite 4 电影院
+# invite 4 游乐园
+
+class game(object):
     def __init__(self):
         self.monthly_money = 1000  # 每月零花钱
         self.event_log = []  # 事件记录
         self.date = datetime.datetime(2015, 8, 31)  # 游戏开始日期 （开学第一天）
 
-    def initialize_player(self, name, gender, difficulty,sub_type):
-        self.player = mainPlayer(name, gender, difficulty,sub_type)
+    def initialize_player(self, name, gender, difficulty, sub_type):
+        self.player = mainPlayer(name, gender, difficulty, sub_type)
 
     def oneDayPass(self):
         copy_date = copy.copy(self.date)
@@ -37,15 +67,16 @@ class game(object):
     def final_test(self):
         final_test = datetime.datetime(2016, 6, 7)
 
-    def event_interpreter(self,):
+    def event_interpreter(self):
         pass
 
-    # initialize_
+    def start(self):
+        pass
 
 
 def main():
     ge = game()
-    ge.initialize_player("Leon", "male", "normal",'art')
+    ge.initialize_player("Leon", "male", "normal", 'art')
     print(ge.player.get_character_status())
 
     # for i in range(0, 30):
