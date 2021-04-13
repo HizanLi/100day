f150cb4054ad27bb986a4bbb3f6adc187ac077b9
HizanLi <linxinl@uvic.ca>
2021-04-12 23:26:56 -0700

    123

--- src/Event/limit/buyBook.py
+++ src/Event/limit/buyBook.py
@@ -7,5 +7,12 @@ from src.Event.mainEvent import mainEvent
 class buyBook(mainEvent):
     event_type = 2
     #use iter method to return story
-    def get_event_info(self, date, character, info):
-        super(buyBook, self).get_event_info(date,character,info)
+    def get_event_info(self, date, character):
+        pass
+
+    def __iter__(self):
+        script = ["今天是阳光明媚的一天",
+                  "你乘坐地铁2号线去了附近的书城",
+                  "",]
+        for line in script:
+            yield line.split(",")
